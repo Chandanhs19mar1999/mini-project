0ae182ce5fc90a903883da7fb076f72c98550d85
chandanhs19mar1999 <chandanhs.19mar1999@gmail.com>
2020-01-27 22:52:18 +0530

    ark

--- miniproject1/views.py
+++ miniproject1/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    print(request.body)
+    #print(request.body)
     return HttpResponse("hello world")
 # Create your views here.
